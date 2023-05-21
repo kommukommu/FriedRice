b8236aefacc70a01949b952849c451453d68cf38
221900304 <zhilinchen2@foxmail.com>
2023-05-21 11:06:48 +0800

    initialise project

---
+++ BackendFriedRice/src/test/java/com/friedrice/backendfriedrice/BackendFriedRiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friedrice.backendfriedrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFriedRiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
